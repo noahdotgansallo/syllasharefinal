d9700e22966e385ef3b7aff15e468d968436d5a5
itedi <jllpickering@gmail.com>
2013-12-02 21:16:51 -0500

    Update sendchat.php

--- sendchat.php
+++ sendchat.php
@@ -6,9 +6,11 @@ $chatid = $_GET['c'];
 $message = $_GET['m'];
 $userid = $_SESSION['id'];
 
+echo "You said:<br>", $message;
+
 $query = "INSERT INTO messages VALUES('', $chatid, '$message', $userid, '', NOW())";
 $run = mysqli_query($connect, $query);
 
-echo "You said:<br>", $message;
+
 
 ?>
